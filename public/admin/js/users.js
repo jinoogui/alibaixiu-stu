2596e8e9d13a2d4b67d4a428a653b4ac44fb8d4f
jinno <1481650619@qq.com>
2019-08-16 15:34:13 +0800

    用户功能完成

--- public/admin/js/users.js
+++ public/admin/js/users.js
@@ -8,25 +8,26 @@ $.ajax({
     })
     //添加用户功能 已完成 8.15 
 $('#addusers').on('submit', function() {
-    var fromdate = $(this).serialize();
-    console.log(fromdate);
+        var fromdate = $(this).serialize();
+        console.log(fromdate);
 
-    $.ajax({
-        url: '/users',
-        type: 'post',
-        data: fromdate,
-        success: function() {
-            location.reload();
-            // alert('xx')
-        },
-        error: function(err) {
-            console.log(err);
-            alert('添加用户失败 请联系管理员')
+        $.ajax({
+            url: '/users',
+            type: 'post',
+            data: fromdate,
+            success: function() {
+                location.reload();
+                // alert('xx')
+            },
+            error: function(err) {
+                console.log(err);
+                alert('添加用户失败 请联系管理员')
 
-        }
+            }
+        })
+        return false;
     })
-    return false;
-})
+    //头像上传功能
 $('#addusers').on('change', '#avatar', function() {
     var fromdata = new FormData();
     fromdata.append('avatar', this.files[0]);
@@ -42,3 +43,27 @@ $('#addusers').on('change', '#avatar', function() {
         }
     })
 });
+$('#tbodyTpl').on('click', '#change', function() {
+    $.ajax({
+        type: 'get',
+        url: '/users/' + $(this).data('id'),
+        success: function(result) {
+            var html = template('changeTpl', result);
+            $('#userlis').html(html);
+        }
+    })
+});
+$('#userlis').on('submit', '#changeusers', function() {
+
+    var fromdata = $(this).serialize();
+    var id = $(this).data('id');
+    $.ajax({
+        url: '/users/' + id,
+        type: 'put',
+        data: fromdata,
+        success: function() {
+            location.reload()
+        }
+    })
+    return false;
+})
